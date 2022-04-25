251371f4cd4cecd6dc1c5f70c23c8054c36978dc
miguelrang <maiky_260101@hotmail.com>
2022-04-25 09:52:52 -0500

    '8'

--- main.py
+++ main.py
@@ -49,9 +49,9 @@ class main(MDApp):
 			
 	def on_start(self):
 		Window.size = 500, 700
-		Window.left = (1400 - 1100)*1.3
-		Window.top = 30
-		self.root.current = "mod"
+		Window.left = (1400 - 1100)*2#1.3
+		Window.top = 200#30
+		self.root.current = "add"
 
 def sqlCONNECTION():
 	try:
